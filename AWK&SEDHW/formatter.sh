6c0e0e02a224a5a28183453bcf63cc2e7f0f2809
fastening <drew.mitchell@okstate.edu>
2019-11-24 13:21:17 -0600

    formatter_for_csv_files_homework

---
+++ AWK&SEDHW/formatter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CSV_File=$1
+Current_Delimeter=$2
+Replacement_Delimeter=$3
+
+echo "USE LIKE THIS: ./formatter.sh CSV_File Current_Delimeter Replacement_Delimeter"
+
+awk '{print $0}' "$CSV_File"  | tr -s "$Current_Delimeter" "$Replacement_Delimeter"
+
+echo "DONE"
